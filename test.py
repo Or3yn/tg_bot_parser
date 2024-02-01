bc00c1c2a69905d25f7cb3c7613c420643e7eecb
Or3yn <zxcbogdanova@gmail.com>
2024-02-01 14:49:26 +0300

    echo bot(initial commit)

--- test.py
+++ test.py
@@ -2,10 +2,9 @@ import telebot
 from telethon.sync import TelegramClient
 from env import *
 
-token='6937176910:AAGFpaSxXwSXDaep8Qxlu1mx3qjGF4h5JK8'
 bot = telebot.TeleBot(token)
-id_channel = -1002123157145
-client = TelegramClient(phone, api_id, api_hash)
+session_name = "NewsCollector"
+client = TelegramClient(session_name, api_id, api_hash)
 client.start()
 
 
